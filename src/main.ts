4b13d40427c7277c67813e1691c512b4fd879a17
tangyanbiao <tangyanbiao@xeno.com>
2022-09-23 17:26:57 +0800

    feat: update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { ConfigKey } from './common/constants/config-key.constant'
 import { ExceptionsFilter } from './common/filters/exception.filter'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useGlobalFilters(new ExceptionsFilter())
+    const configService = app.get(ConfigService)
     const documentOptions = new DocumentBuilder()
         .addBearerAuth()
         .setTitle('API 文档')
@@ -13,6 +16,6 @@ async function bootstrap() {
         .build()
     const document = SwaggerModule.createDocument(app, documentOptions)
     SwaggerModule.setup('docs', app, document)
-    await app.listen(3000)
+    await app.listen(configService.get<number>(ConfigKey.SERVER_PORT))
 }
 bootstrap()
